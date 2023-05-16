ef5fbc6712ba21448792c1efc6c8256b3a70719e
Firat Özcan <firat.oezcan@gmail.com>
2023-05-16 20:14:57 +0200

    Add sentry

--- src/RankingTable.tsx
+++ src/RankingTable.tsx
@@ -14,6 +14,18 @@ import { IconChevronDown, IconChevronUp } from "@supabase/ui";
 import { MoreVert } from "@mui/icons-material";
 import { useQueryClient } from "@tanstack/react-query";
 
+type Drinks = {
+  amount_ml: number;
+  percentage: number;
+}[];
+
+const calculateTotalAlcohol = (drinks: Drinks) =>
+  drinks.reduce((total, drink) => {
+    const alcoholInMilliliters = (drink.amount_ml * drink.percentage) / 100;
+    const alcoholInGrams = alcoholInMilliliters * 0.789; // Assuming the density of alcohol is 0.789 g/mL
+    return total + alcoholInGrams;
+  }, 0);
+
 export const RankingTable = () => {
   const { data: drinks, isLoading } = useSupabaseQuery((supabase) => supabase.from("drink").select("*"), { refetchInterval: 10000, refetchOnWindowFocus: true });
   const { data: users, isLoading: isLoadingUsers } = useSupabaseQuery((supabase) => supabase.from("user").select("*"), { refetchInterval: 10000, refetchOnWindowFocus: true });
@@ -81,80 +93,73 @@ export const RankingTable = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {Object.entries(drinksByUser).map(([userId, userDrinks]) => (
-                <>
-                  <TableRow key={userId} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                    <TableCell component="th" scope="row">
-                      <IconButton color="primary" onClick={() => setVisibleDetails(visibleDetails === userId ? undefined : userId)}>
-                        {visibleDetails ? <IconChevronUp /> : <IconChevronDown />}
-                      </IconButton>
-                    </TableCell>
-                    <TableCell component="th" scope="row">
-                      {users.find((u) => u.user_id === userId)?.name ?? "Nicht registriert (geil ey)"}
-                    </TableCell>
-                    <TableCell>
-                      {userDrinks
-                        .reduce((total, drink) => {
-                          const alcoholInMilliliters = (drink.amount_ml * drink.percentage) / 100;
-                          const alcoholInGrams = alcoholInMilliliters * 0.789; // Assuming the density of alcohol is 0.789 g/mL
-                          return total + alcoholInGrams;
-                        }, 0)
-                        .toFixed(2)}
-                      g
-                    </TableCell>
-                  </TableRow>
-                  <TableRow sx={{ border: 0 }}>
-                    <TableCell colSpan={8} sx={{ py: 0 }}>
-                      <Collapse in={visibleDetails === userId} mountOnEnter>
-                        <Stack spacing={2}>
-                          <Box>
-                            <Typography variant="h6">Statistik:</Typography>
-                            <Typography variant="body2">Kurze: {userDrinks.filter((d) => d.type === "shot").length}</Typography>
-                            <Typography variant="body2">0.33l Bier: {userDrinks.filter((d) => d.type === "beer" && d.amount_ml === 330).length}</Typography>
-                            <Typography variant="body2">0.5l Bier: {userDrinks.filter((d) => d.type === "beer" && d.amount_ml === 500).length}</Typography>
-                            <Typography variant="body2">
-                              Anderes Bier: {userDrinks.filter((d) => d.type === "beer" && d.amount_ml !== 330 && d.amount_ml !== 500).length}
-                            </Typography>
-                            <Typography variant="body2">Mischen: {userDrinks.filter((d) => d.type === "mixed").length}</Typography>
-                          </Box>
-                          <Box>
-                            <Typography variant="h6">Auflistung:</Typography>
-                            <Table sx={{ maxWidth: 444 }}>
-                              <TableHead>
-                                <TableRow>
-                                  <TableCell></TableCell>
-                                  <TableCell>Getränk</TableCell>
-                                  <TableCell align="right">Menge</TableCell>
-                                  <TableCell align="right">Prozent</TableCell>
-                                </TableRow>
-                              </TableHead>
-                              <TableBody>
-                                {userDrinks.map((drink, index) => (
-                                  <TableRow key={index} onContextMenu={console.log}>
-                                    <TableCell>
-                                      <IconButton onClick={handleClick} sx={{ p: 0 }}>
-                                        <MoreVert />
-                                      </IconButton>
-                                      <Menu anchorEl={anchorEl} open={open} onClose={handleClose} MenuListProps={{ sx: { p: 0 } }}>
-                                        <Button color="error" size="small" variant="contained" onClick={hasClickedDelete ? handleDeleteDrink(drink.id) : handleDeleteFirstClick}>
-                                          {hasClickedDelete ? "Wirklich löschen?" : "Löschen"}
-                                        </Button>
-                                      </Menu>
-                                    </TableCell>
-                                    <TableCell>{DrinkMapping[drink.type]}</TableCell>
-                                    <TableCell align="right">{drink.amount_ml}ml</TableCell>
-                                    <TableCell align="right">{drink.percentage}%</TableCell>
+              {Object.entries(drinksByUser)
+                .sort((a, b) => calculateTotalAlcohol(b[1]) - calculateTotalAlcohol(a[1]))
+                .map(([userId, userDrinks]) => (
+                  <>
+                    <TableRow key={userId} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                      <TableCell component="th" scope="row">
+                        <IconButton color="primary" onClick={() => setVisibleDetails(visibleDetails === userId ? undefined : userId)}>
+                          {visibleDetails ? <IconChevronUp /> : <IconChevronDown />}
+                        </IconButton>
+                      </TableCell>
+                      <TableCell component="th" scope="row">
+                        {users.find((u) => u.user_id === userId)?.name ?? "Nicht registriert (geil ey)"}
+                      </TableCell>
+                      <TableCell>{calculateTotalAlcohol(userDrinks).toFixed(2)}g</TableCell>
+                    </TableRow>
+                    <TableRow sx={{ border: 0 }}>
+                      <TableCell colSpan={8} sx={{ py: 0 }}>
+                        <Collapse in={visibleDetails === userId} mountOnEnter>
+                          <Stack spacing={2}>
+                            <Box>
+                              <Typography variant="h6">Statistik:</Typography>
+                              <Typography variant="body2">Kurze: {userDrinks.filter((d) => d.type === "shot").length}</Typography>
+                              <Typography variant="body2">0.33l Bier: {userDrinks.filter((d) => d.type === "beer" && d.amount_ml === 330).length}</Typography>
+                              <Typography variant="body2">0.5l Bier: {userDrinks.filter((d) => d.type === "beer" && d.amount_ml === 500).length}</Typography>
+                              <Typography variant="body2">
+                                Anderes Bier: {userDrinks.filter((d) => d.type === "beer" && d.amount_ml !== 330 && d.amount_ml !== 500).length}
+                              </Typography>
+                              <Typography variant="body2">Mischen: {userDrinks.filter((d) => d.type === "mixed").length}</Typography>
+                            </Box>
+                            <Box>
+                              <Typography variant="h6">Auflistung:</Typography>
+                              <Table sx={{ maxWidth: 444 }}>
+                                <TableHead>
+                                  <TableRow>
+                                    <TableCell></TableCell>
+                                    <TableCell>Getränk</TableCell>
+                                    <TableCell align="right">Menge</TableCell>
+                                    <TableCell align="right">Prozent</TableCell>
                                   </TableRow>
-                                ))}
-                              </TableBody>
-                            </Table>
-                          </Box>
-                        </Stack>
-                      </Collapse>
-                    </TableCell>
-                  </TableRow>
-                </>
-              ))}
+                                </TableHead>
+                                <TableBody>
+                                  {userDrinks.map((drink, index) => (
+                                    <TableRow key={index} onContextMenu={console.log}>
+                                      <TableCell>
+                                        <IconButton onClick={handleClick} sx={{ p: 0 }}>
+                                          <MoreVert />
+                                        </IconButton>
+                                        <Menu anchorEl={anchorEl} open={open} onClose={handleClose} MenuListProps={{ sx: { p: 0 } }}>
+                                          <Button color="error" size="small" variant="contained" onClick={hasClickedDelete ? handleDeleteDrink(drink.id) : handleDeleteFirstClick}>
+                                            {hasClickedDelete ? "Wirklich löschen?" : "Löschen"}
+                                          </Button>
+                                        </Menu>
+                                      </TableCell>
+                                      <TableCell>{DrinkMapping[drink.type]}</TableCell>
+                                      <TableCell align="right">{drink.amount_ml}ml</TableCell>
+                                      <TableCell align="right">{drink.percentage}%</TableCell>
+                                    </TableRow>
+                                  ))}
+                                </TableBody>
+                              </Table>
+                            </Box>
+                          </Stack>
+                        </Collapse>
+                      </TableCell>
+                    </TableRow>
+                  </>
+                ))}
             </TableBody>
           </Table>
           <Box display="flex" justifyContent="flex-end" px={1} py={2}>
